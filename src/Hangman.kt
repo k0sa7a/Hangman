0cd0559021d2f8649e9e76626b824804a8d6684c
Stefan Filev <sfilev@expediagroup.com>
2022-10-10 10:01:50 +0100

    Initial setup for Hangman game: List of words, guesses allowed, mistakes and guesses made

--- src/Hangman.kt
+++ src/Hangman.kt
@@ -1,3 +1,18 @@
-fun main(args: Array<String>) {
+import java.util.*
+import kotlin.random.Random
 
+val words = listOf("shoe", "jacket", "diver", "muffin", "spear", "truck", "nickel", "coal", "yellow", "grain")
+var word = ""
+val guesses = arrayListOf<Char>()
+var remainingGuesses = 6
+var mistakes = 0
+
+fun main(args: Array<String>) {
+    setupGame()
+}
+
+fun setupGame(){
+    val wordIndex = Random.nextInt(words.size)
+    word = words[wordIndex].uppercase(Locale.getDefault())
+    println(word)
 }
