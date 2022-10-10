3f2ec25f3cb7ec787457ee291a9da937dcffec58
Stefan Filev <sfilev@expediagroup.com>
2022-10-10 10:27:25 +0100

    Added visual word-art for different mistake numbers 0-6

--- src/Hangman.kt
+++ src/Hangman.kt
@@ -15,4 +15,95 @@ fun setupGame(){
     val wordIndex = Random.nextInt(words.size)
     word = words[wordIndex].uppercase(Locale.getDefault())
     println(word)
+    
+    printGameStatus()
+}
+
+fun printGameStatus(){
+    when(mistakes) {
+        0 -> print0Mistakes()
+        1 -> print1Mistake()
+        2 -> print2Mistakes()
+        3 -> print3Mistakes()
+        4 -> print4Mistakes()
+        5 -> print5Mistakes()
+        6 -> print6Mistakes()
+    }
+}
+
+fun print0Mistakes(){
+    println("  |------|-")
+    println("  |      | ")
+    println("  |        ")
+    println("  |        ")
+    println("  |        ")
+    println("  |        ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print1Mistake(){
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      O ")
+    println("  |        ")
+    println("  |        ")
+    println("  |        ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print2Mistakes(){
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      O ")
+    println("  |      | ")
+    println("  |      | ")
+    println("  |        ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print3Mistakes(){
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      O ")
+    println("  |     /| ")
+    println("  |      | ")
+    println("  |        ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print4Mistakes(){
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      O ")
+    println("  |     /|\\")
+    println("  |      | ")
+    println("  |        ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print5Mistakes(){
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      O ")
+    println("  |     /|\\")
+    println("  |      | ")
+    println("  |     /  ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print6Mistakes(){
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      O ")
+    println("  |     /|\\")
+    println("  |      | ")
+    println("  |     / \\")
+    println(" /|\\      ")
+    println("/ | \\     ")
 }
